c37b509f30233694beec91d0256b3e784cf99992
Erik Holmberg <ermh95@gmail.com>
2020-12-15 13:39:07 +0100

    Optimized Day15, now runs in 0.55s

--- Day15/Day15.java
+++ Day15/Day15.java
@@ -8,7 +8,10 @@ public class Day15 {
 	public static void main(String[] args) throws FileNotFoundException{
 		File input = new File("C:\\Users\\sterho\\eclipse-workspace\\AoC_2020_01\\Day15\\input15.txt");
 		System.out.println(part1(input));
+		long startTime = System.nanoTime();
 		System.out.println(part2(input));
+		long stopTime = System.nanoTime();
+		System.out.println(stopTime - startTime);
 	}
 	
 	private static long part1(File input) throws FileNotFoundException{
@@ -19,7 +22,39 @@ public class Day15 {
 		return findNumber(30000000, input);
 	}
 	
-	private static long findNumber (long endpoint, File input) throws FileNotFoundException{
+	private static int findNumber (int endpoint, File input) throws FileNotFoundException{
+		Scanner sc = new Scanner(input);
+		int[] numbersSaid = new int[endpoint];
+		for(int i = 0; i < endpoint; i++) {
+			numbersSaid[i] = -1;
+		}
+		int i = 0;
+		int lastNumber = 0;
+		int tmp, lastOccurance;
+		while(sc.hasNextInt()) {
+			lastNumber = sc.nextInt();
+			if(sc.hasNextInt()) {
+				numbersSaid[lastNumber] = i;
+			}
+			i++;
+		}
+		sc.close();
+		while(i < endpoint) {
+			lastOccurance = numbersSaid[lastNumber];
+			if(lastOccurance == -1) {
+				numbersSaid[lastNumber] = i-1;
+				lastNumber = 0;
+			} else {
+				tmp = i - 1 - lastOccurance;
+				numbersSaid[lastNumber] = i-1;
+				lastNumber = tmp;
+			}
+			i++;
+		}
+		return lastNumber;
+	}
+	
+	private static long findNumberSlow (long endpoint, File input) throws FileNotFoundException{
 		Scanner sc = new Scanner(input);
 		HashMap<Long, Long> numbersSaid = new HashMap<Long, Long>();
 		long i = 0;
